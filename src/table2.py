044f06c8789130d69039a8d7acf04d8e010aaa2c
Basso42 <91699102+Basso42@users.noreply.github.com>
2025-02-18 11:29:26 +0100

    typo

--- src/table2.py
+++ src/table2.py
@@ -29,7 +29,7 @@ def run_regression(data, formula, weight_col=None):
     return model
 
 
-# Filtering dataset
+# Filters
 base_filter = (df_country['asia'] != 1) & (df_country['date'] > 1200) & (df_country['date'] < 1900)
 data_filtered = df_country[base_filter]
 
@@ -37,12 +37,12 @@ data_filtered = df_country[base_filter]
 Panel A: flexible specification - MATCHING RESULTS
 """
 
-# Define regression formulas
+# formulas
 base_formula = "sjurbanization ~ westerneurope1600 + westerneurope1700 + westerneurope1750 + westerneurope1800 + westerneurope1850 + "
 base_formula += " + ".join([f'country{i}' for i in range(1, 30)]) + " + "
 base_formula += " + ".join([f'yr{i}' for i in [1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1750, 1800, 1850]])
 
-# Running regressions based on Stata replication
+# Running regressions
 regressions = {}
 regressions['col1'] = run_regression(data_filtered, base_formula, weight_col='totalpopulation')
 data_filtered2 = df_country[(df_country['asia'] != 1) & (df_country['date'] < 1900)]
@@ -64,13 +64,13 @@ regressions['col6'] = run_regression(data_with_asia, formula_atlantic, weight_co
 data_no_britain = data_filtered[data_filtered['countryid'] != 7]
 regressions['col7'] = run_regression(data_no_britain, formula_atlantic, weight_col='totalpopulation')
 
-# Using coast-to-area trade variables
+# coast-to-area trade variables
 formula_coast = base_formula + " + coasttoarea1500 + coasttoarea1600 + coasttoarea1700 + coasttoarea1750 + coasttoarea1800 + coasttoarea1850"
 regressions['col8'] = run_regression(data_filtered, formula_coast, weight_col='totalpopulation')
 regressions['col9'] = run_regression(data_filtered2, formula_coast, weight_col='totalpopulation')
 regressions['col10'] = run_regression(data_filtered, formula_coast)
 
-# Display results
+# results
 for col, model in regressions.items():
     print(f"Results for {col}:")
     print(model.summary())
@@ -80,22 +80,22 @@ for col, model in regressions.items():
 """
 Panel B: structured specification - MATCHING RESULTS
 """
-# Define variables
+# variables
 western_vars = [f'westerneurope{year}' for year in [1600, 1700, 1750, 1800, 1850]]
 country_vars = [f'country{i}' for i in range(1, 30)]
 year_vars = [f'yr{i}' for i in [1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1750, 1800, 1850]]
 trade_vars = ["logsept2002atradexatlantictrader", "logsept2002atradexcoasttoarea"]
 
-# Define filters
+# filters
 base_filter = (df_country["asia"] != 1) & (df_country["date"] > 1200) & (df_country["date"] < 1900)
 base_filter_extended = (df_country["asia"] != 1) & (df_country["date"] < 1900)
 
-# Define regression formulas
+# regression formulas
 formula_1 = "sjurbanization ~ " + " + ".join(western_vars + country_vars + year_vars)
 formula_trade = formula_1 + " + " + trade_vars[0]
 formula_trade_coast = formula_1 + " + " + trade_vars[1]
 
-# Run regressions
+# regressions
 regressions = {
     "Column 1": weighted_regression(formula_1, df_country[base_filter], "totalpopulation"),
     "Column 2": weighted_regression(formula_1, df_country[base_filter_extended], "totalpopulation"),
@@ -109,7 +109,7 @@ regressions = {
     "Column 10": unweighted_regression(formula_trade_coast, df_country[base_filter & df_country["totalpopulation"].notna()]),
 }
 
-# Display results
+# results
 for col, model in regressions.items():
     print(f"Results for {col}:")
     print(model.summary())
